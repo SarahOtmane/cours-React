fd6c533818c71d70a3b1252c3e810d02c6dc9698
SarahOtmane <sarahotmane02@gamil.com>
2023-02-06 11:56:16 +0100

    06/02/23

--- src/InputSpinner/InputSpinner.js
+++ src/InputSpinner/InputSpinner.js
@@ -5,18 +5,18 @@ const InputSpinner = () => {
 
     const [count, setCount] = useState(0);
 
-    const onUpdate = (num)=>{
-        setCount(count+num);
+    const onUpdate = (value)=>{
+        setCount((prev) => prev+value);
     }
 
     return ( 
         <div className="stepper horizontal">
-        <img onClick={ (event =>onUpdate(-1)) } src="https://alikinvv.github.io/stepper/build/img/arrow.svg" className="arrow top" alt="" />
-        <div className="box">
-            <div className="numbers1">{count}</div>
+            <img onClick={ (event =>onUpdate(-1)) } src="https://alikinvv.github.io/stepper/build/img/arrow.svg" className="arrow top" alt="" />
+            <div className="box">
+                <div className="numbers1">{count}</div>
+            </div>
+            <img onClick={ (event =>onUpdate(1)) } src="https://alikinvv.github.io/stepper/build/img/arrow.svg" className="arrow bottom" alt="" />
         </div>
-        <img onClick={ (event =>onUpdate(1)) } src="https://alikinvv.github.io/stepper/build/img/arrow.svg" className="arrow bottom" alt="" />
-    </div>
     );
 }
  
